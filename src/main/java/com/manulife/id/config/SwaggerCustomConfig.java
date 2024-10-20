a3bb20f5341a4143da8f5f878d446c63ee20e461
hafizosina <hafizosina@gmail.com>
2024-10-20 14:40:29 +0700

    setup swagger

---
+++ src/main/java/com/manulife/id/config/SwaggerCustomConfig.java
@@ -0,0 +1,9 @@
+package com.manulife.id.config;
+
+import org.springdoc.webmvc.ui.SwaggerConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerCustomConfig extends SwaggerConfig {
+    // setup for later
+}
