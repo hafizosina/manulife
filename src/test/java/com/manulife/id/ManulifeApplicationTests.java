e4d3a8e6a37aaae64a079694bbda47cc33859a9f
hafizosina <hafizosina@gmail.com>
2024-10-20 13:47:34 +0700

    first commit and skeleton

---
+++ src/test/java/com/manulife/id/ManulifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manulife.id;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManulifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
