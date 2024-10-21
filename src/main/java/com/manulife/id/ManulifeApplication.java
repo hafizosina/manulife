a3e7f72bcd38a2fc7da93093a2d9b3196009ce40
hafizosina <hafizosina@gmail.com>
2024-10-21 10:11:46 +0700

    open h2 database connection

--- src/main/java/com/manulife/id/ManulifeApplication.java
+++ src/main/java/com/manulife/id/ManulifeApplication.java
@@ -1,13 +1,24 @@
 package com.manulife.id;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
+
 
 @SpringBootApplication
 public class ManulifeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManulifeApplication.class, args);
+	}
+
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server inMemoryH2DatabaseaServer() throws SQLException {
+		return Server.createTcpServer(
+				"-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
 	}
 
 }
